ffe5fc848759555ad47495db077d3f4c23af8fc5
Aleksandar Gjoreski <aleksandar.gjoreski@condexo.it>
2021-04-11 15:08:30 +0200

    Revert "dataloader improvement"
    This reverts commit a8c04fe11864107e708cbe347e09918ea6c01de2.

--- src/Graph/Player/loaders.ts
+++ src/Graph/Player/loaders.ts
@@ -1,16 +1,9 @@
 import DataLoader from 'dataloader'
 import { mongoPlayer } from '../../MongoDB/Player'
-import { Player } from '../../MongoDB/Player/Entities'
 
-const batchPlayers = async (ids: string[]) => {
-  const players = await mongoPlayer.getPlayersByIds(ids)
-  const playerMap = players.reduce<{ [_id: string]: Player }>((acc, player) => {
-    return {
-      ...acc,
-      [String(player._id)]: player
-    }
-  }, {})
-  return ids.map(_id => playerMap[_id])
-}
+/** Loaders */
 
-export const playerLoader = new DataLoader(batchPlayers)
+export const playerLoader = new DataLoader((playerIds: string []) => {
+  const promises = playerIds.map(_id => mongoPlayer.getPlayerById(_id))
+  return Promise.all(promises)
+})
--- src/Graph/User/loaders.ts
+++ src/Graph/User/loaders.ts
@@ -1,16 +1,9 @@
 import DataLoader from 'dataloader'
 import { mongoUser } from '../../MongoDB/User'
-import { User } from '../../MongoDB/User/Entities'
 
-const batchUsers = async (ids: string[]) => {
-  const users = await mongoUser.getUserByIds(ids)
-  const userMap = users.reduce<{ [_id: string]: User }>((acc, user) => {
-    return {
-      ...acc,
-      [String(user._id)]: user
-    }
-  }, {})
-  return ids.map(_id => userMap[_id])
-}
+/** Loaders */
 
-export const userLoader = new DataLoader(batchUsers)
+export const userLoader = new DataLoader((userIds: string[]) => {
+  const promises = userIds.map(_id => mongoUser.getUserById(_id))
+  return Promise.all(promises)
+})
