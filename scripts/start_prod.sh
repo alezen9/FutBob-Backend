3aa0ee452989894fda7282582da16bb93a31adba
Aleksandar Gjoreski <aleksandar.gjoreski@condexo.it>
2021-10-02 01:22:23 +0200

    minor

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -2,6 +2,9 @@
 yellow=`tput setaf 3`
 reset=`tput sgr0`
 
+# install packages
+echo "${yellow}[futbob] Installing packages...${reset}"
+yarn
 # compile typescript
 echo "${yellow}[futbob] Transpiling...${reset}"
 tsc
@@ -13,3 +16,6 @@ rsync -avum --include='*' './public/' './dist/public'
 # start server with pm2
 echo "${yellow}[futbob] Starting app...${reset}"
 pm2 start dist/index.js
+# save pm2 snapshot
+echo "${yellow}[futbob] Saving pm2 snapshot...${reset}"
+pm2 save
